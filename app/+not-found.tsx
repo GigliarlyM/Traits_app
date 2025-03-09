5010f2ed119bae9b279d1c366be8fa3c0063b5d4
Gigliárly Marcelino <gigliarlymarcelino@gmail.com>
2025-03-09 14:32:22 -0300

    correcao de um erro

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,14 +1,13 @@
 import { Link, Stack } from 'expo-router';
 import { StyleSheet, View, Text } from 'react-native';
 
-
 export default function NotFoundScreen() {
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
         <Text >This screen doesn't exist.</Text>
-        <Link href='/(tabs)/home' style={styles.link}>
+        <Link href='/(tabs)' style={styles.link}>
           <Text >Go to home screen!</Text>
         </Link>
         <Link href='/auth/login' style={styles.link}>
