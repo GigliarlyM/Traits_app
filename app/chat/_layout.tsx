e569c8d733853bee908f6b2d80fecd459247ebf3
GIGLIARLY MARCELINO GONZAGA <gigliarly.gonzaga@maisunifacisa.com.br>
2025-04-01 10:16:27 -0300

    add func chat

---
+++ app/chat/_layout.tsx
@@ -0,0 +1,15 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+
+
+export default function TabLayout() {
+  return (
+
+    <Stack>
+      <Stack.Screen name='index' options={{ headerShown: false }} />
+      <Stack.Screen name="chatIa" options={{ headerShown: false }} />
+      <Stack.Screen name="chat" options={{ headerShown: false }} />
+    </Stack>
+
+  );
+}
---
+++ app/service/socket.ts
@@ -0,0 +1,51 @@
+import WebSocket from 'ws'
+
+interface ChatMessage {
+    type: 'message' | 'error' | 'join' | 'leave';
+    user: string;
+    message: string;
+    to?: string;
+    timestamp?: Date;
+}
+
+let newSocket: WebSocket | null = null
+
+export const getConnection = () => {
+    newSocket = new WebSocket('ws://localhost:8080')
+
+    newSocket.onopen = () => {
+        console.log("Conexao estabelecida!!")
+    }
+
+    newSocket.onerror = (error) => {
+        console.log("Erro WebSocket ", error);
+    }
+
+    return newSocket
+}
+
+export const receiveMessage = () => {
+
+    if (newSocket) {
+        newSocket.onmessage = (event: any) => {
+            console.log('Messagem recebida: ', event.data)
+            return event.data
+        }
+    }
+}
+
+export const sendMessage = (message: ChatMessage) => {
+    if (newSocket) {
+        newSocket.send(
+            JSON.stringify(message)
+        )
+    }
+}
+
+export const exitConnection = () => {
+    if (newSocket) {
+        newSocket.close = () => {
+            console.log("Conexao WebSocket fechada!")
+        }
+    }
+}
